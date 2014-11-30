2a26d84db0ed706778ef2305792f0d5daea5143d
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-11-30 11:45:25 +0100

    Per user normalization

--- analysis/recommendation/exploratoryAnalysisRecommendation.m
+++ analysis/recommendation/exploratoryAnalysisRecommendation.m
@@ -9,8 +9,8 @@ load('./data/recommendation/songTrain.mat');
 
 % Counts matrix:
 % Each (i, j) corresponds to the listening count of user i for artist j
-R = Ytrain;
-[uIdx, aIdx] = find(R);
+Y = Ytrain;
+[uIdx, aIdx] = find(Y);
 % Indices of users having at least one data example
 usersIdx = unique(uIdx);
 % Indices of artists having at least one listen
@@ -26,25 +26,18 @@ artistsIdx = unique(aIdx);
 % Each (i, j) corresponds to a friendship relationship between users i and j
 friendships = Gtrain;
 
-%% Normalization
-% "Make the data more Gaussian"
-% Counts are positive integers only, so it would violate our Gaussian
-% distribution assumption.
-
-% TODO
-
 %% Artists visualization
 
 % Listening counts for one (user, artist) span from 1 to 352698
-[minCount, idxMin] = min(nonzeros(R))
-[maxCount, idxMax] = max(nonzeros(R))
+[minCount, idxMin] = min(nonzeros(Y))
+[maxCount, idxMax] = max(nonzeros(Y))
 
 % There seems to be a major outlier:
 % (user 385, artist 9162) = 352698
 % This is a reminder that we should remove outliers.
 
 % Total listening count for an artist spans from 1 to 2274039
-countsPerArtist = sum(R(:, artistsIdx), 1); % Only for nonzero artists
+countsPerArtist = sum(Y(:, artistsIdx), 1); % Only for nonzero artists
 [minArtistCount, idxMin] = min(countsPerArtist)
 [maxArtistCount, idxMax] = max(countsPerArtist)
 % The single most "popular" artist is Britney Spears
@@ -52,6 +45,51 @@ countsPerArtist = sum(R(:, artistsIdx), 1); % Only for nonzero artists
 % Simple model: average count for an artist / average count for all artists
 % This is a simple estimate of popularity
 
-%% Features analysis
 
+%% Normalization
+% "Make the data more Gaussian"
+% Counts are positive integers only, so it would violate our Gaussian
+% distribution assumption.
+
+% We normalize *among user*, that is we make users lie on the same scale (a
+% heavy user will have counts comparable to a light user). This way, we
+% retain the artists popularity, which is important information.
+% TODO: is it the correct way? Wouldn't we want to know if the user is very
+% active, in order to predict accurately? (If we keep the factors used at
+% normalization, we can rescale afterwards)
+meanPerUser = zeros(size(usersIdx, 1), 1);
+deviationPerUser = zeros(size(usersIdx, 1), 1);
+values = zeros(nnz(Y), 1);
+for i = 1:length(usersIdx)
+    %[thisUIdx, thisAIdx] = find(Y(i, :));
+    counts = nonzeros(Y(i, :));
+    meanPerUser(i) = mean(counts);
+    deviationPerUser(i) = std(counts);
+    
+    values(uIdx == usersIdx(i)) = (counts - meanPerUser(i)) ./ deviationPerUser(i);
+end;
+% Generate a new normalized sparse matrix
+Ynormalized = sparse(uIdx, aIdx, values, size(Y, 1), size(Y, 2));
+
+% TODO: apply the same factors to the test set
+% TODO: denormalize after prediction to obtain the correct scale
+
+clear i counts thisUserIdx values;
+
+%% Verify the result of normalization
+normalizedCounts = nonzeros(Ynormalized);
+hist(normalizedCounts);
+
+% Mean should be 0, deviation should be 1
+for i = 1:length(usersIdx)
+    counts = nonzeros(Ynormalized(i, :));
+    mean(counts)
+    std(counts)
+end;
+
+%% Outliers removal
+% TODO
+% TODO: also need to handle artists who have 0 listenings
+
+%% Features analysis
 % TODO
