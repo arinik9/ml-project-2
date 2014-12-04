b6de6212010681fa5d235f40f191c6f16726eafd
Jade Copet <jade.copet@gmail.com>
2014-12-04 16:54:28 +0100

    GP large scale classification

--- detection/compareMethods.m
+++ detection/compareMethods.m
@@ -105,14 +105,53 @@ hyp = minimize(hyp, @gp, -100, @infFITC, meanfunc, covfuncF, likfunc, x, y);
 yhatGP = outputLabelsFromPrediction(m, 0);
 
 
-%% GP Classification
-% TODO: Large Scale Classification to implement
+%% GP large scale Classification
+
+x = Tr.X(1:500,:);
+y = Tr.y(1:500);
+t = Te.X(1:500,:);
+n = size(x,1);
+
+
+% Mean function
+meanfunc = @meanConst; 
+hyp.mean = 0;
+
+% use inducing points u and to base the computations on cross-covariances 
+% between training, test and inducing points only
+nu = fix(n); iu = randperm(n); iu = iu(1:nu); u = x(iu,:);
+
+% Covariance function
+covfunc = @covSEiso; 
+ell = 1.0; % characteristic length-scale
+sf = 1.0; % standard deviation of the signal sf
+hyp.cov = log([ell sf]);
+covfuncF = {@covFITC, {covfunc}, u};
+
+% Likelihood function
+likfunc = @likErf;
+
+inffunc = @infFITC_EP;                       % also @infFITC_Laplace is possible
+
+hyp = minimize(hyp, @gp, -40, inffunc, meanfunc, covfuncF, likfunc, x, y);
+[a, b, c, d, lp] = gp(hyp, inffunc, meanfunc, covfuncF, likfunc, x, y, t, ones(n,1));
 
 % Note: Output arguments: 
 % When computing test probabilities, we call gp with additional test inputs, 
 % and as the last argument a vector of targets for which the log probabilities 
 % lp should be computed. The fist four output arguments of the function are mean 
 % and variance for the targets and corresponding latent variables respectively.
+
+% lp gives the logarithm probabilities. Get the exp to have predictions
+gpPred = exp(lp);
+yHatGP = outputLabelsFromPrediction(gpPred, 0.5);
+
+% We do recover our large rate of negative VS positive images
+hist(yHatGP);
+
+res = fastROC(Te.y(1:500) > 0, gpPred);
+
+% TODO : Play with parameters + ROC Curve
 
 %% Random Predictions
 
