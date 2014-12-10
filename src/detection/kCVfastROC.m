a9023d60eac1211da437b1afa9d856a9696f3a46
Jade Copet <jade.copet@gmail.com>
2014-12-10 13:15:43 +0100

    jbFill in kCVfastROC

--- src/detection/kCVfastROC.m
+++ src/detection/kCVfastROC.m
@@ -1,4 +1,4 @@
-function [tprAtWP,auc,fprAvg,tprAvg] = kCVfastROC(allLabels, allScores, plot_flag, plotStyle)
+function [tprAtWPAvg,aucAvg,fprAvg,tprAvg] = kCVfastROC(allLabels, allScores, plot_flag, plotStyle)
 
     if ~exist('plot_flag','var')
         plot_flag = 0;
@@ -8,6 +8,7 @@ function [tprAtWP,auc,fprAvg,tprAvg] = kCVfastROC(allLabels, allScores, plot_fla
         plotStyle = 'b';
     end
     
+    % Compute the fpr and tpr means to draw average curve
     n = size(allLabels,2);
     nPoints = size(allLabels,1);
     tprAtWP = zeros(n,1);
@@ -15,16 +16,26 @@ function [tprAtWP,auc,fprAvg,tprAvg] = kCVfastROC(allLabels, allScores, plot_fla
     fpr = zeros(nPoints, n);
     tpr = zeros(nPoints, n);
     for i=1:n
-        % allLabels(:,i);
         [tprAtWP(i),auc(i),fpr(:,i),tpr(:,i)] = fastROC(allLabels(:,i) > 0, allScores(:,i), 0);
     end
+    
+    % Compute average area under curve and tprAtWP
+    aucAvg = mean(auc,2);
+    tprAtWPAvg = mean(tprAtWP,2);
+    
+    % Compute average false positive and true positive rate
     fprAvg = mean(fpr,2);
     tprAvg = mean(tpr,2);
-
+    
+    % Compute standard deviation
+    uncertScore = std(tpr,0, 2);
+    
     %Plot the ROC curve
     if plot_flag==1
+        uncert = 2*uncertScore; % +/- 2 sigma => 95% confidence interval
         figure();
         semilogx(fprAvg,tprAvg,plotStyle,'LineWidth',2); hold on;
+        jbfill(fprAvg, tprAvg + uncert, tprAvg - uncert, 'b', 'b', 1, 0.2);
         %plot(fprAvg,tprAvg,plotStyle,'LineWidth',2);
         xlabel('False Positive Rate');
         ylabel('True Positive Rate');
