52ed4fb73bf14fce45a892eace4946dc80089cad
Jade Copet <jade.copet@gmail.com>
2014-12-11 23:19:32 +0100

    kcv find params on RF + fix on NN

--- src/detection/findnTreesRF.m
+++ src/detection/findnTreesRF.m
@@ -1,39 +1,39 @@
-function [dropOutStar, trainTPR, testTPR] = findDropoutNeuralNetwork(y, X, k, dropOutFractions, seed)
+function [nTreesStar, trainTPR, testTPR] = findnTreesRF(y, X, k, nTreesValues, seed)
 
-    nDf = length(dropOutFractions);
+    nDf = length(nTreesValues);
 
     trainTPR = zeros(nDf,1);
     testTPR = zeros(nDf,1);
     bestTPR = 0;
-    dropOutStar = 0;
+    nTreesStar = 0;
     
     for i=1:nDf
         
-        dpFraction = dropOutFractions(i);
+        nTrees = nTreesValues(i);
         
-        learn = @(y, X) trainNeuralNetwork(y, X, 0, 1, 'sigm', dpFraction, 0);
-        predict = @(model, X) predictNeuralNetwork(model, X);
+        learn = @(y, X) trainRandomForest(y, X, nTrees);
+        predict = @(model, X) predictRandomForest(model, X);
         computePerformances = @(trueOutputs, pred, model_name) kCVfastROC(trueOutputs, pred, model_name, 0);
         
         setSeed(seed);
         [trainTPR(i), testTPR(i)] = kFoldCrossValidation(y, X, k, learn, predict, computePerformances);
         
         if (testTPR(i) > bestTPR)
-            dropOutStar = dpFraction;
+            nTreesStar = nTrees;
             bestTPR = testTPR(i);
         end
         
         % Status
-        fprintf('TPR for dropout = %f: train %f | test %f\n', dpFraction, trainTPR(i), testTPR(i));
+        fprintf('TPR for nTrees = %f: train %f | test %f\n', nTrees, trainTPR(i), testTPR(i));
         
     end
     
     % Plot evolution of train and test error with respect to lambda
     figure;
-    semilogx(dropOutFractions, trainTPR, '.-b');
+    plot(nTreesValues, trainTPR, '.-b');
     hold on;
-    semilogx(dropOutFractions, testTPR, '.-r');
-    xlabel('Drop out');
+    plot(nTreesValues, testTPR, '.-r');
+    xlabel('number of Trees');
     ylabel('Training (blue) and test (red) TPR');
     
 end
