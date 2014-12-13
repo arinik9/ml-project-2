10e93ab65404b8c6fef56d8651df465e1324f0cf
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-13 12:10:23 +0100

    Reco: pretty generic functions for learning and predictions

--- analysis/recommendation/predictionWorkflow.m
+++ analysis/recommendation/predictionWorkflow.m
@@ -3,7 +3,7 @@ clearvars;
 loadDataset;
 
 %% Any kind of processing
-[Ynorm, newMean] = normalizedSparse(Ytrain);
+normalizedMean = mean(nonzeros(Ytrain));
 
 %% Output predictions
 % For each user
@@ -13,25 +13,25 @@ for i = 1:sz.u
     ii = (idx.tr.u == i);
     if(nnz(ii) > 0)
         jj = idx.tr.a(ii);
-        trValues(ii) = Ynorm(i, jj) + 0.05;
-    end
-    
+        trValues(ii) = Ytrain(i, jj) + 0.05;
+    end;
+
     ii = (idx.te.u == i);
     if(nnz(ii) > 0)
         jj = idx.te.a(ii);
-        teValues(ii) = newMean * rand(1, length(jj));
-    end
-end
+        teValues(ii) = normalizedMean * rand(1, length(jj));
+    end;
+end;
 
 trYhatFamous = sparse(idx.tr.u, idx.tr.a, trValues, sz.u, sz.a);
 teYhatFamous = sparse(idx.te.u, idx.te.a, teValues, sz.u, sz.a);
 
-% Compute error on denormalized data
-e.tr.famous = computeRmse(Ytrain, denormalize(trYhatFamous, idx));
-e.te.famous = computeRmse(Ytest, denormalize(teYhatFamous, testIdx));
+% Compute error on normalized data
+e.tr.famous = computeRmse(Ytrain, trYhatFamous);
+e.te.famous = computeRmse(Ytest, teYhatFamous);
 
 fprintf('RMSE on popular artists only : %f | %f\n', e.tr.famous, e.te.famous);
-diagnoseError(Ytrain, denormalize(trYhatFamous, idx));
-diagnoseError(Ytest, denormalize(teYhatFamous, testIdx));
+diagnoseError(Ytrain, trYhatFamous);
+diagnoseError(Ytest, teYhatFamous);
 
 
---
+++ src/recommendation/evaluateMethod.m
@@ -0,0 +1,32 @@
+function [trError, teError] = evaluateMethod(getPredictor, Ytrain, Ytest, userDV, artistDV)
+% EVALUATEMETHOD
+%
+% INPUT
+%   getPredictor: function(Y, Ytest)
+%                 which returns a `predict(user, artist)` function
+%   Y
+%   Ytest
+%   userDV:       precomputed derived variables
+%   artistDV:     precomputed derived variables
+% OUTPUT
+%   trError, teError: estimated train and test errors
+
+    % TODO: run on mutliple random seeds
+    % TODO: need more input?
+    predictor = getPredictor(Ytrain, Ytest, userDV, artistDV);
+
+    % Predict training set
+    [trIdx, trSz] = getRelevantIndices(Ytrain);
+    trYhat = predictCounts(predictor, trIdx, trSz);
+    trError = computeRmse(Ytrain, trYhat);
+
+    % Predict test set
+    [teIdx, teSz] = getRelevantIndices(Ytest);
+    teYhat = predictCounts(predictor, teIdx, teSz);
+    teError = computeRmse(Ytest, teYhat);
+
+
+    % fprintf('RMSE for method TODO: %f | %f\n', trError, teError);
+    diagnoseError(Ytrain, trYhat);
+    diagnoseError(Ytest, teYhat);
+end
