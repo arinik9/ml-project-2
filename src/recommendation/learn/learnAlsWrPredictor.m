1f6ce61aeb97fc082385ee3accfa1121d4ba469d
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-13 21:46:06 +0100

    Reco: ALSWR biais correction

--- src/recommendation/learn/learnAlsWrPredictor.m
+++ src/recommendation/learn/learnAlsWrPredictor.m
@@ -11,5 +11,13 @@ function predictor = learnAlsWrPredictor(Y, Ytest, userDV, artistDV, nFeatures, 
     % TODO: auto-learn lambda
 
     [U, M] = alswr(Y, Ytest, nFeatures, lambda, displayLearningCurve);
-    predictor = @(user, artist) U(:, user)' * M(:, artist);
+    
+    % Biais correction
+    % TODO: do we really have the right to use the test set's mean
+    % for post-processing?
+    [idx, sz] = getRelevantIndices(Ytest);
+    Yhat = reconstructFromLowRank(U, M, idx, sz);
+    biais = mean(nonzeros(Yhat)) - mean(nonzeros(Ytest));
+    
+    predictor = @(user, artist) U(:, user)' * M(:, artist) - biais;
 end
