f89f657d8309f931ba2383e5d9346d84b62c0d5e
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-13 18:41:12 +0100

    Reco: handle tail very simply

--- src/recommendation/learn/learnHeadTailPredictor.m
+++ src/recommendation/learn/learnHeadTailPredictor.m
@@ -12,14 +12,33 @@ function predictor = learnHeadTailPredictor(Y, Ytest, userDV, artistDV, headThre
 %   Park, Yoon-Joo, and Alexander Tuzhilin.
 %   "The long tail of recommender systems and how to leverage it."
 
-
     % TODO: take the social network in input!
-    Gtrain = sparse(size(Y, 1), size(Y, 1));
+    G = sparse(size(Y, 1), size(Y, 1));
 
+    headPredictor = learnHeadPredictor(Y, G, Ytest, userDV, artistDV, headThreshold);
+    tailPredictor = learnTailPredictor(Y, G, Ytest, userDV, artistDV);
+    predictor = @(user, artist) predict(user, artist, artistDV(:, 2), headThreshold, headPredictor, tailPredictor);
+end
+
+function prediction = predict(user, artist, popularity, headThreshold, head, tail)
+% At prediction time, choose either head or tail predictor
+    if(popularity(artist) > headThreshold)
+        prediction = head(user, artist);
+    else
+        prediction = tail(user, artist);
+    end;
+end
+
+function predictor = learnHeadPredictor(Y, G, Ytest, userDV, artistDV, headThreshold)
     % Each prediction uses a model learnt from all users having listened to the artist
-    betas = modelEachArtist(Y, Gtrain, headThreshold, userDV, artistDV);
-    getFeatures = @(user, artist) generateFeatures(artist, user, Gtrain, userDV, artistDV);
+    % TODO: `modelEachArtist` shouldn't be aware of `headThreshold`
+    betas = modelEachArtist(Y, G, headThreshold, userDV, artistDV);
+    getFeatures = @(user, artist) generateFeatures(artist, user, G, userDV, artistDV);
 
-    % TODO: handle the tail! This takes care of the head only
     predictor = @(user, artist) getFeatures(user, artist) * betas(:, artist);
+end
+
+function predictor = learnTailPredictor(Y, G, Ytest, userDV, artistDV)
+    % TODO: handle the tail properly!
+    predictor = learnAveragePerArtistPredictor(Y, Ytest, userDV, artistDV);
 end
