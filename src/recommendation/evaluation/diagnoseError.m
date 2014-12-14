1ca1f827bd97a8e8acd488604cffd2a9639a38c7
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-14 13:31:00 +0100

    Reco: `diagnoseError` shows difference of norm per user

--- src/recommendation/evaluation/diagnoseError.m
+++ src/recommendation/evaluation/diagnoseError.m
@@ -8,6 +8,7 @@ function [errors, teErrors] = diagnoseError(y, yHat, yTest, yTestHat)
 % OUTPUT:
 %   errors: [number of available listening count, average RMSE error made] (sorted)
 
+
     % Baseline
     baseline = sparse(size(y, 1), size(y, 2));
     [averagedBaseline, ~] = computeErrorByCount(y, baseline);
@@ -16,6 +17,7 @@ function [errors, teErrors] = diagnoseError(y, yHat, yTest, yTestHat)
     nRows = 1;
     name = ' ';
     [averaged, errors] = computeErrorByCount(y, yHat);
+    errorByUser = computeErrorByUser(y, yHat);
 
     % Test set
     if(exist('yTest', 'var'))
@@ -23,26 +25,36 @@ function [errors, teErrors] = diagnoseError(y, yHat, yTest, yTestHat)
         name = ' train ';
         [teAveraged, teErrors] = computeErrorByCount(yTest, yTestHat, y);
         [teAveragedBaseline, ~] = computeErrorByCount(yTest, baseline, y);
+        teErrorByUser = computeErrorByUser(yTest, yTestHat);
     end;
 
     figure;
-    makeSubplots(name, errors, averaged, averagedBaseline, nRows, 0);
+    makeSubplots(name, errors, averaged, errorByUser, averagedBaseline, nRows, 0);
 
     if(exist('yTest', 'var'))
-        makeSubplots(' test ', teErrors, teAveraged, teAveragedBaseline, nRows, 2);
+        makeSubplots(' test ', teErrors, teAveraged, teErrorByUser, teAveragedBaseline, nRows, 3);
     end;
 end
 
-function makeSubplots(name, errors, averaged, averagedBaseline, nRows, offset)
+function makeSubplots(name, errors, averaged, errorByUser, averagedBaseline, nRows, offset)
     nCounts = unique(errors(:, 1));
 
+    % Plot error for each user
+    subplot(nRows, 3, offset + 1);
+    semilogy(1:size(errorByUser, 1), errorByUser, '.');
+    xlim([1 size(errorByUser, 1)+1]);
+    xlabel('Users');
+    ylabel('Euclidean norm of nonzero entries (log scale)');
+    legend('True norm', 'Predicted norm');
+    title([name, 'error over each user']);
+
     % Plot repartition of log(RMSE)
-    subplot(nRows, 2, offset + 1);
+    subplot(nRows, 3, offset + 2);
     hist(errors(:, 2), 20);
     title(['Repartition of', name, 'RMSE']);
 
     % Plot error made VS quantity of data available
-    subplot(nRows, 2, offset + 2);
+    subplot(nRows, 3, offset + 3);
     hold on;
     %errorbar(1:n, averaged(:, 1), averaged(:, 2));
     semilogx(nCounts, averaged(:, 1), 'b.');
