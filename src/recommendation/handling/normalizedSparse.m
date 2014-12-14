e988abd39d8f0529cfa5dc033bfe476d185fb524
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-14 11:27:05 +0100

    Reco: normalization now centers the data

--- src/recommendation/handling/normalizedSparse.m
+++ src/recommendation/handling/normalizedSparse.m
@@ -1,12 +1,16 @@
-function [Ynormalized, newMean] = normalizedSparse(Y)
+function [Ynormalized, newMean, newStd] = normalizedSparse(Y)
 % NORMALIZEDSPARSE Gaussianize the data by applying the log transform
-    % Add a tiny noise offset
+    
+    [idx, sz] = getRelevantIndices(Y);
+    
+    logCounts = log(nonzeros(Y));
+    newMean = mean(logCounts);
+    newStd = mean(logCounts);
+    
+    % We add a tiny noise offset
     % To avoid log(1) yielding new 0 values (which would then be
     % interpreted as unknown).
-    logCounts = log(nonzeros(Y)) + 1e-8;
-    newMean = mean(logCounts);
-
-    [N, D] = size(Y);
-    [uIdx, aIdx] = find(Y);
-    Ynormalized = sparse(uIdx, aIdx, logCounts, N, D);
+    normalizedValues = (logCounts - newMean + 1e-8) ./ 1; %newStd;
+    
+    Ynormalized = sparse(idx.u, idx.a, normalizedValues, sz.u, sz.a);
 end
